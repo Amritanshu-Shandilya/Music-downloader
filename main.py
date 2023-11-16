73b98af366c8cb87cf88e08a6e3a58dabb132005
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2023-11-16 19:50:54 +0000

    First working version (works for 1 song only)

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import keys
 import spotipy
-import json
+import requests
 from pprint import pprint
 from spotipy.oauth2 import SpotifyClientCredentials
+from pytube import Search, YouTube
 
 
 user_name = 'Shiv'
 client_id = keys.CLIENT_ID
 client_Secret = keys.SECRET_KEY
 redirect_uri = 'http://google.com/callback/' 
+
+yt_query = 'https://www.youtube.com/watch?v='
 
 playlist_link = "https://open.spotify.com/playlist/3qpQXtFqUeR2c9HeQoP6ES?si=mbXKhy0dQ_qKFXa3-OBIkQ"
 
@@ -26,11 +29,43 @@ def get_tracks():
     for track in spotify_obj.playlist_items(playlist_uri)["items"]:
         track_uri = track["track"]["uri"]
         track_name = track["track"]["name"]
-        result = track_name, spotify_obj.audio_features(track_uri)
+        result = track_name, track_uri.split(':')[2]
         tracks.append(result)
 
     return tracks
 
+def get_isrc_id(test_id):
+    # Converts Spotify song id to ISRC id
+    song_data = spotify_obj.track(test_id)
+    return song_data['external_ids']['isrc']
 
-raw_data = get_tracks()
-print(raw_data[0][0]+" : "+raw_data[0][1][0]['id'])
+
+
+# pprint(get_tracks())
+tracks = get_tracks()
+test_id = tracks[0][1]
+isrc_id = get_isrc_id(test_id)
+
+
+# Search the video on Youtube using isrc id
+search_obj = Search(isrc_id)
+videoId = str(search_obj.results).split('=')[1].rstrip('>]')
+
+# Search the video on youtube using link formed using videoId
+yt_query+=str(videoId)
+yt_object = YouTube(yt_query)
+
+# Filter the desired Stream object which is in audio format
+thing = str(yt_object.streams.filter(mime_type='audio/mp4')[0])
+
+# Extracting the tag from the Stream object
+tag = (((((thing.rstrip('>')).lstrip('<')).split(' ')[1]).split('=')[1])).strip('"')
+
+# Download the video using the tag
+audio = yt_object.streams.get_by_itag(tag)  
+audio.download()
+print("Audio downlaoded!")
+
+
+
+
