953bc960bd3df2142332e3e05f3c5d1cb00a58b2
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2023-11-18 04:46:50 +0000

    Added code to deal with whole playlist

--- main.py
+++ main.py
@@ -41,29 +41,30 @@ def get_isrc_id(test_id):
 
 # print(get_tracks())
 tracks = get_tracks()
-test_id = tracks[0][1]
-isrc_id = get_isrc_id(test_id)
+for track in tracks:
+    print('Track Name -> '+track[1])
+    # generating isrc id
+    isrc_id = get_isrc_id(track[1])
+    # search the video using isrc id
+    search_obj = Search(isrc_id)
+    # generating video id
+    video_id = str(search_obj.results).split('=')[1].rstrip('>]')
 
+    # create yt-search link
+    yt_query += str(video_id)
+    
+    # Search the video using the link formed
+    yt_object = YouTube(yt_query)
 
-# Search the video on Youtube using isrc id
-search_obj = Search(isrc_id)
-videoId = str(search_obj.results).split('=')[1].rstrip('>]')
+    # Filter the desired Stream Object
+    stream = str(yt_object.streams.filter(mime_type='audio/mp4')[0])
+    
+    # Extract the tag id
+    tag_id = (((((stream.rstrip('>')).lstrip('<')).split(' ')[1]).split('=')[1])).strip('"')
 
-# Search the video on youtube using link formed using videoId
-yt_query+=str(videoId)
-yt_object = YouTube(yt_query)
-
-# Filter the desired Stream object which is in audio format
-thing = str(yt_object.streams.filter(mime_type='audio/mp4')[0])
-
-# Extracting the tag from the Stream object
-tag = (((((thing.rstrip('>')).lstrip('<')).split(' ')[1]).split('=')[1])).strip('"')
-
-# Download the video using the tag
-audio = yt_object.streams.get_by_itag(tag)  
-audio.download()
-print("Audio downlaoded!")
-
-
+    # Download the video using the tag
+    audio = yt_object.streams.get_by_itag(tag_id)  
+    audio.download()
+    print("Audio downlaoded!")
 
 
